0018c9927b6b87fd0d91c848b938484b1dc94360
Zywczas <zywczas@o2.pl>
2020-11-04 09:00:55 +0100

    added MainFragment.kt, nav graph core done

---
+++ app/src/main/java/com/zywczas/recipemaster/views/MainFragment.kt
@@ -0,0 +1,13 @@
+package com.zywczas.recipemaster.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zywczas.recipemaster.R
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+
+}
