392381a4621446a079e19d29c4eaca291a9e202a
Zywczas <zywczas@o2.pl>
2020-11-04 12:50:12 +0100

    speed dial working

--- app/src/main/java/com/zywczas/recipemaster/views/LoginFragment.kt
+++ app/src/main/java/com/zywczas/recipemaster/views/LoginFragment.kt
@@ -15,11 +15,28 @@ class LoginFragment @Inject constructor() : Fragment(R.layout.fragment_login) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupOnClickListeners()
+        setupUi()
+
+    }
+
+    private fun setupUi(){
+        speed_dial_login.inflate(R.menu.menu_speed_dial)
     }
 
     private fun setupOnClickListeners(){
-        fab_main_login.setOnClickListener {
-            showToast("dziala")
+        speed_dial_login.setOnActionSelectedListener { item ->
+            when(item.id){
+                R.id.get_recipe_item -> {
+                    showToast("przepisy dzialaja")
+                    true
+                }
+                R.id.facebook_item -> {
+                    showToast("fejs dziala, yoohu!!")
+                    true
+                }
+                else -> false
+            }
         }
+
     }
 }
