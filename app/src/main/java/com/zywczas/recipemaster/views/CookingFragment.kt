a4bb20afcd96204b28843b7eed182fa2e7e276b2
Zywczas <zywczas@o2.pl>
2020-11-06 19:43:15 +0100

    saving photos working

--- app/src/main/java/com/zywczas/recipemaster/views/CookingFragment.kt
+++ app/src/main/java/com/zywczas/recipemaster/views/CookingFragment.kt
@@ -1,17 +1,23 @@
 package com.zywczas.recipemaster.views
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ContentValues
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -28,20 +34,50 @@ import com.zywczas.recipemaster.viewmodels.UniversalViewModelFactory
 import kotlinx.android.synthetic.main.fragment_cooking.*
 import java.io.File
 import java.io.FileOutputStream
-import java.lang.Exception
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
 
 class CookingFragment @Inject constructor(
-    private val viewModelFactory : UniversalViewModelFactory,
-    private val glide : RequestManager
-) : Fragment(R.layout.fragment_cooking), SaveImageDialog.SaveImageListener {
+    private val viewModelFactory: UniversalViewModelFactory,
+    private val glide: RequestManager
+) : Fragment(R.layout.fragment_cooking) {
 
-    private val viewModel : CookingViewModel by viewModels { viewModelFactory }
+    private val viewModel: CookingViewModel by viewModels { viewModelFactory }
     private val userName by lazyAndroid { requireArguments().let { CookingFragmentArgs.fromBundle(it).userName } }
     private val appBarConfig by lazyAndroid { AppBarConfiguration(setOf(R.id.destination_login)) }
     private var recipe: Recipe? = null
+    private var arePermissionsGranted = false
+    private val storageRequestCode by lazyAndroid { 777 }
+    private val permissions by lazyAndroid {
+        arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
+    private var requestedImageUrl: String? = null
+    private val photoUrlKey by lazy { "photo key" }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        verifyStoragePermissions()
+        requestedImageUrl = savedInstanceState?.getString(photoUrlKey)
+    }
+
+    private fun verifyStoragePermissions() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                permissions[0]
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                permissions[1]
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            arePermissionsGranted = true
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -53,7 +89,7 @@ class CookingFragment @Inject constructor(
         toolbar_cooking.setupWithNavController(findNavController(), appBarConfig)
     }
 
-    private fun setupObserver(){
+    private fun setupObserver() {
         viewModel.recipe.observe(viewLifecycleOwner) { resource ->
             showProgressBar(false)
             when (resource.status) {
@@ -66,15 +102,16 @@ class CookingFragment @Inject constructor(
         }
     }
 
-    private fun showProgressBar(visible : Boolean){
+    private fun showProgressBar(visible: Boolean) {
         progressBar_cooking.isVisible = visible
     }
 
     @SuppressLint("SetTextI18n")
-    private fun updateUI(recipe: Recipe){
+    private fun updateUI(recipe: Recipe) {
         recipe.title?.let {
             toolbar_cooking.title = "$it ${getString(R.string.recipe)}"
-            food_name_textView.text = "$it:" }
+            food_name_textView.text = "$it:"
+        }
         food_description_textView.text = recipe.description
         ingredients_list_textView.text = recipe.ingredientsDescription
         preparing_steps_textView.text = recipe.preparingDescription
@@ -92,11 +129,11 @@ class CookingFragment @Inject constructor(
         }
     }
 
-    private fun showError(msg: Event<Int>){
+    private fun showError(msg: Event<Int>) {
         msg.getContentIfNotHandled()?.let { showSnackbar(getString(it)) }
     }
 
-    private fun showSnackbar(msg: String){
+    private fun showSnackbar(msg: String) {
         val color = ContextCompat.getColor(requireContext(), R.color.darkGrey)
         val snackbar = Snackbar.make(requireView(), msg, Snackbar.LENGTH_LONG)
             .setBackgroundTint(color)
@@ -106,107 +143,189 @@ class CookingFragment @Inject constructor(
         snackbar.show()
     }
 
-    private fun getRecipeOnViewModelInit(){
+    private fun getRecipeOnViewModelInit() {
         showProgressBar(true)
         viewModel.getRecipeOnViewModelInit()
     }
 
-    private fun setupOnClickListeners(){
+    private fun setupOnClickListeners() {
         food0_imageView.setOnClickListener(imageClickListener)
         food1_imageView.setOnClickListener(imageClickListener)
         food2_imageView.setOnClickListener(imageClickListener)
     }
 
     private val imageClickListener = View.OnClickListener {
-        val viewIndex = it.tag as Int
-        areYouSureDialog(viewIndex)
-    }
-
-    private fun areYouSureDialog(viewIndex : Int){
-        val dialog = SaveImageDialog()
-        val bundle = Bundle()
-        bundle.putInt(EXTRA_VIEW_INDEX, viewIndex)
-        dialog.arguments = bundle
-        dialog.show(childFragmentManager, "SaveImage")
-    }
-
-    override fun saveImage(index: Int) {
-        if (recipe?.images?.get(index) == null){
+        val index = getIndex(it)
+        if (recipe?.images?.get(index) == null) {
             showSnackbar(getString(R.string.no_image_to_save))
         } else {
-            val url = recipe!!.images!![index]
-            saveImageToExternalStorage(url)
+            requestedImageUrl = recipe!!.images!![index]
+            areYouSureDialog()
         }
     }
-//todo jak juz bedzie dzialac to sprobowac wrzucic to w dialog
-    private fun saveImageToExternalStorage(url: String){
-        val dirPath = Environment.getExternalStorageDirectory().absolutePath + "/" //todo poprawic...
-        val dir = File(dirPath)
+
+    private fun getIndex(view: View): Int =
+        when (view.id) {
+            food0_imageView.id -> 0
+            food1_imageView.id -> 1
+            food2_imageView.id -> 2
+            else -> 666
+        }
+
+    private fun areYouSureDialog() {
+        val builder = AlertDialog.Builder(requireContext()).create()
+        val dialogView = layoutInflater.inflate(R.layout.dialog_save_image, null)
+        val yes = dialogView.findViewById<TextView>(R.id.yes_textView_dialog)
+        val no = dialogView.findViewById<TextView>(R.id.no_textView_dialog)
+        yes.setOnClickListener {
+            checkPermissionsAndSaveImage()
+            builder.dismiss()
+        }
+        no.setOnClickListener { builder.dismiss() }
+        builder.setView(dialogView)
+        builder.show()
+    }
+
+    private fun checkPermissionsAndSaveImage() {
+        if (arePermissionsGranted) {
+            downloadAndSaveImage()
+        } else {
+            askForPermissionsAndSavePhoto()
+        }
+    }
+
+    private fun downloadAndSaveImage() {
+        showProgressBar(true)
         val timeStamp = SimpleDateFormat("yyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val fileName = timeStamp + "_" + url.substring(url.lastIndexOf("/") + 1)
-        glide.load(url).into(object : CustomTarget<Drawable>(){
-            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
-                showProgressBar(true)
-                val bitmap = resource.toBitmap()
-                saveToStorage(bitmap, dir, fileName)
+        val photoName = requestedImageUrl!!.substring(requestedImageUrl!!.lastIndexOf("/") + 1)
+        val fileName = "${timeStamp}_$photoName"
+        glide.load(requestedImageUrl)
+            .into(object : CustomTarget<Drawable>() {
+                override fun onResourceReady(resource: Drawable,transition: Transition<in Drawable>?) {
+                    val bitmap = resource.toBitmap()
+                    saveImageToGallery(bitmap, fileName)
+                }
 
-//                val resolver = context?.applicationContext?.contentResolver
-//
-//                if (resolver == null) {
-//                    //todo
-//                    showToast("nie ma contextu...")
-//                    logD("nie ma contextu...")
-//                } else {
-//                    val imageCollection = MediaStore.Images.Media.getContentUri(
-//                        MediaStore.VOLUME_EXTERNAL
-//                    )
-//                }
-            }
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
 
-            override fun onLoadCleared(placeholder: Drawable?) {
-            }
-
-            override fun onLoadFailed(errorDrawable: Drawable?) {
-                showSnackbar(getString(R.string.image_saving_error))
-            }
-        })
+                override fun onLoadFailed(errorDrawable: Drawable?) {
+                    showProgressBar(false)
+                    showSnackbar(getString(R.string.image_saving_error))
+                }
+            })
     }
 
-    private fun saveToStorage(image: Bitmap, storageDir: File, fileName: String){
-        val imageFile = File(storageDir, fileName)
-        val savedImagePath = imageFile.absolutePath
-        try {
-            val out = FileOutputStream(imageFile)
-            image.compress(Bitmap.CompressFormat.JPEG, 100, out)
-            out.close()
-            //todo dac tutaj kolejny dialog - Success
-            showSnackbar(getString(R.string.image_saved))
-            showProgressBar(false)
-        } catch (e: Exception){
-            showSnackbar(getString(R.string.image_saving_error))
-            logD(e)
+    private fun saveImageToGallery(bitmap: Bitmap, fileName: String) {
+        var fos: OutputStream? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            context?.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory((Environment.DIRECTORY_PICTURES))
+            val image = File(imagesDir, fileName)
+            fos = FileOutputStream(image)
         }
-
+        fos?.use { bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it) }  //todo tutaj dopiero jest zapisywanie
+        showSnackbar(getString(R.string.image_saved))
+        showProgressBar(false)
     }
 
 
+    private fun saveToStorage(image: Bitmap, fos: OutputStream) {
+        image.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        fos.close()
+        showSnackbar(getString(R.string.image_saved))
+        showProgressBar(false)
 
+    }
 
+//todo jak juz bedzie dzialac to sprobowac wrzucic to w dialog
+//    private fun saveImageToGallery(){
+//        val dirPath = Environment.getExternalStorageDirectory().absolutePath + "/" //todo poprawic...
+//        val dir = File(dirPath)
+//        val timeStamp = SimpleDateFormat("yyMMdd_HHmmss", Locale.getDefault()).format(Date())
+//        val photoName = requestedImageUrl!!.substring(requestedImageUrl!!.lastIndexOf("/") + 1)
+//        val fileName = "${timeStamp}_$photoName"
+//        glide.load(requestedImageUrl).into(object : CustomTarget<Drawable>(){
+//            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+//                showProgressBar(true)
+//                val bitmap = resource.toBitmap()
+//                saveToStorage(bitmap, dir, fileName)
+//
+////                val resolver = context?.applicationContext?.contentResolver
+////
+////                if (resolver == null) {
+////                    //todo
+////                    showToast("nie ma contextu...")
+////                    logD("nie ma contextu...")
+////                } else {
+////                    val imageCollection = MediaStore.Images.Media.getContentUri(
+////                        MediaStore.VOLUME_EXTERNAL
+////                    )
+////                }
+//            }
+//
+//            override fun onLoadCleared(placeholder: Drawable?) {
+//            }
+//
+//            override fun onLoadFailed(errorDrawable: Drawable?) {
+//                showSnackbar(getString(R.string.image_saving_error))
+//            }
+//        })
+//    }
 
+//    private fun saveToStorage(image: Bitmap, storageDir: File, fileName: String){
+//        val imageFile = File(storageDir, fileName)
+//        val savedImagePath = imageFile.absolutePath
+//        logD(savedImagePath)
+//        try {
+//            val out = FileOutputStream(imageFile)
+//            image.compress(Bitmap.CompressFormat.JPEG, 100, out)
+//            out.close()
+//            //todo dac tutaj kolejny dialog - Success
+//            showSnackbar(getString(R.string.image_saved))
+//            showProgressBar(false)
+//        } catch (e: Exception){
+//            showProgressBar(false)
+//            showSnackbar(getString(R.string.image_saving_error))
+//            logD(e)
+//        }
+//    }
 
+    private fun askForPermissionsAndSavePhoto() {
+        requestPermissions(permissions, storageRequestCode)
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            storageRequestCode -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //todo raczej nie potrzebne arePermissionsGranted = true
+                    downloadAndSaveImage()
+                } else {
+                    logD("permission odmowione: ${permissions[1]}")
+                    showToast(getString(R.string.permission_warning))
+                }
+            }
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        requestedImageUrl?.let { outState.putString(photoUrlKey, it) }
+    }
 
 
 }
