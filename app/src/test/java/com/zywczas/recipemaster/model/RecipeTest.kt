459cd02704cbbfe9328571d54a92c2510cd58b13
Zywczas <zywczas@o2.pl>
2020-11-07 14:31:37 +0100

    RecipeTest.kt finished

--- app/src/test/java/com/zywczas/recipemaster/model/RecipeTest.kt
+++ app/src/test/java/com/zywczas/recipemaster/model/RecipeTest.kt
@@ -1,29 +1,44 @@
 package com.zywczas.recipemaster.model
 
-import com.zywczas.recipemaster.util.MOCKED_API_RESPONSE_BODY
 import com.zywczas.recipemaster.util.TestUtil
-import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.assertEquals
-import retrofit2.HttpException
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.net.HttpURLConnection
 import org.junit.jupiter.api.Assertions.*
 
 internal class RecipeTest {
 
+    lateinit var recipe: Recipe
+
+    @BeforeEach
+    fun setup(){
+        recipe = Recipe(
+            "Pizza",
+            "Pizza jest potrawą kuchni włoskiej, obecnie szeroko rozpowszechnioną...",
+            listOf("3 szklanki mąki pszennej", "1 łyżeczka soli", "przyprawy do smaku (oregano, bazylia i słodka papryka)"),
+            listOf("Suche składniki dokładnie mieszamy.", "Drożdże zalawamy ciepłą wodą, olejem i cukrem. Odstawiamy do wyrośnięcia.", "Gotowy płyn wlewamy do mąki i mieszam najpierw łyżką, potem zagniatamy ręką."),
+            listOf("http://mooduplabs.com/test/pizza1.jpg", "http://mooduplabs.com/test/pizza2.jpg", "http://mooduplabs.com/test/pizza3.jpg")
+        )
+    }
+
     @Test
     fun convertIngredientsListToDescription(){
         val expected = "- 3 szklanki mąki pszennej\n" +
-                    "- 1 łyżeczka soli\n" +
-                "- przyprawy do smaku (oregano, bazylia i słodka papryka)"
-        val recipe = TestUtil.recipe1
+                                "- 1 łyżeczka soli\n" +
+                                "- przyprawy do smaku (oregano, bazylia i słodka papryka)"
 
         recipe.convertIngredientsListToDescription()
         val actual = recipe.ingredientsDescription
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun convertPreparingStepsToDescription(){
+        val expected = "1. Suche składniki dokładnie mieszamy.\n\n" +
+                "2. Drożdże zalawamy ciepłą wodą, olejem i cukrem. Odstawiamy do wyrośnięcia.\n\n" +
+                "3. Gotowy płyn wlewamy do mąki i mieszam najpierw łyżką, potem zagniatamy ręką."
+
+        recipe.convertPreparingStepsToDescription()
+        val actual = recipe.preparingDescription
 
         assertEquals(expected, actual)
     }
