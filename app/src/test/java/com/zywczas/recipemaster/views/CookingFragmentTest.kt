6fe0ebb8a9cc26a66531c557b6a3da5c9f712121
Zywczas <zywczas@o2.pl>
2020-11-09 13:06:18 +0100

    2 tests running in CookingFragmentTest.kt

--- app/src/test/java/com/zywczas/recipemaster/views/CookingFragmentTest.kt
+++ app/src/test/java/com/zywczas/recipemaster/views/CookingFragmentTest.kt
@@ -7,8 +7,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.bumptech.glide.RequestManager
 import com.zywczas.recipemaster.BaseApplication
@@ -24,6 +23,7 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.unmockkAll
 import io.reactivex.rxjava3.core.Flowable
+import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -52,12 +52,7 @@ class CookingFragmentTest {
         every { viewModelFactory.create(CookingViewModel::class.java) } returns CookingViewModel(
             repo
         )
-        every {
-            fragmentFactory.instantiate(
-                any(),
-                any()
-            )
-        } returns CookingFragment(viewModelFactory, glide)
+        every {fragmentFactory.instantiate(any(),any())} returns CookingFragment(viewModelFactory, glide)
         navController.setGraph(R.navigation.main_nav_graph)
         navController.setCurrentDestination(R.id.destination_cooking)
     }
@@ -93,6 +88,7 @@ class CookingFragmentTest {
             .check(matches(isDisplayed()))
         onView(withId(R.id.preparingSteps_textView_cooking)).perform(scrollTo())
             .check(matches(isDisplayed()))
+        onView(withId(R.id.progressBar_cooking)).check(matches(not(isDisplayed())))
         onView(withId(R.id.imagesTitle_textView_cooking)).perform(scrollTo())
             .check(matches(isDisplayed()))
         onView(withId(R.id.food0_imageView_cooking)).perform(scrollTo())
@@ -100,6 +96,41 @@ class CookingFragmentTest {
         onView(withId(R.id.food1_imageView_cooking)).check(matches(isDisplayed()))
         onView(withId(R.id.food2_imageView_cooking)).perform(scrollTo())
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun isContentDisplayed(){
+        val recipe = TestUtil.recipe1
+        val toolbarTitle = "${recipe.title} ${app.getString(R.string.recipe)}"
+
+        @Suppress("UNUSED_VARIABLE")
+        val scenario = launchFragmentInContainer(
+            fragmentArgs = bundle,
+            themeResId = R.style.AppTheme
+        ) {
+            CookingFragment(viewModelFactory, glide).also { fragment ->
+                fragment.viewLifecycleOwnerLiveData.observeForever { viewLifeCycleOwner ->
+                    if (viewLifeCycleOwner != null) {
+                        Navigation.setViewNavController(fragment.requireView(), navController)
+                    }
+                }
+            }
+        }
+
+        onView(withId(R.id.toolbar_cooking)).check(matches(hasDescendant(withText(toolbarTitle))))
+        onView(withId(R.id.foodName_textView_cooking)).check(matches(withText("${recipe.title}:")))
+        onView(withId(R.id.foodDescription_textView_cooking))
+            .check(matches(withText(recipe.foodDescription)))
+        onView(withId(R.id.ingredientsTitle_textView_cooking))
+            .check(matches(withText(app.getString(R.string.ingredients))))
+        onView(withId(R.id.ingredientsList_textView_cooking)).perform(scrollTo())
+            .check(matches(withText(recipe.ingredientsDescription)))
+        onView(withId(R.id.preparingTitle_textView_cooking)).perform(scrollTo())
+            .check(matches(withText(app.getString(R.string.preparing))))
+        onView(withId(R.id.preparingSteps_textView_cooking)).perform(scrollTo())
+            .check(matches(withText(recipe.preparingDescription)))
+        onView(withId(R.id.imagesTitle_textView_cooking)).perform(scrollTo())
+            .check(matches(withText(app.getString(R.string.images))))
     }
 
 
